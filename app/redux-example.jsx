6dac56a43e0005b50d1baa8ab737ff9621f83695
Manuel <manuel1412@gmail.com>
2017-05-01 18:04:20 +0100

    refactored file to create files for actions, reducers and store

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,137 +3,8 @@ var axios = require('axios');
 
 console.log('Starting redux example');
 
-//-----------------------------------------
-var nameReducer = (state = 'Anonymous', action) => {
-    switch(action.type){
-        case 'CHANGE_NAME':
-            return action.name;
-        default:
-            return state
-    }
-}
-var changeName = (name) => {
-    return {
-        type: 'CHANGE_NAME',
-        name
-        //^ ES6>>> name : name
-    }
-}
-
-
-//-----------------------------------------
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action) => {
-    switch(action.type){
-        case 'ADD_HOBBY':
-            return [
-                ...state, //pulls the current array and add the object below to it
-                {
-                    id: nextHobbyId++,
-                    hobby: action.hobby
-                }]
-        case 'REMOVE_HOBBY':
-            return state.filter((hobby) => hobby.id !== action.id)
-        default:
-            return state
-    }
-}
-var addHobby = (hobby) => {
-    return {
-        type: 'ADD_HOBBY',
-        hobby
-    }
-}
-var removeHobby = (id) => {
-    return {
-        type: 'REMOVE_HOBBY',
-        id
-    }
-}
-
-
-//-----------------------------------------
-var nextMovieId = 1;
-var moviesReducer = (state = [], action) => {
-    switch(action.type){
-        case 'ADD_MOVIE':
-            return [
-                ...state, //pulls the current array and add the object below to it
-                {
-                    id: nextMovieId++,
-                    movie: action.movie,
-                    genre: action.genre
-                }]
-        case 'REMOVE_MOVIE':
-            return state.filter((movie) => movie.id !== action.id)
-        default:
-            return state
-    }
-}
-var addMovie = (movie, genre) =>{
-    return {
-        type: 'ADD_MOVIE',
-        movie,
-        genre
-    }
-}
-var removeMovie = (id) => {
-    return {
-        type: 'REMOVE_MOVIE',
-        id
-    }
-}
-
-
-//-----------------------------------------
-var mapReducer = (state={isFetching:false, url:undefined}, action) => {
-    switch(action.type) {
-        case 'START_LOCATION_FETCH':
-            return {
-                isFetching: true,
-                url: undefined
-            }
-        case 'COMPLETE_LOCATION_FETCH':
-            return {
-                isFetching: false,
-                url: action.url
-            }
-        default:
-            return state
-    }
-}
-var startLocationFetch = () => {
-    return {
-        type: 'START_LOCATION_FETCH'
-    }
-}
-var completeLocationFetch = (url) => {
-    return {
-        type: 'COMPLETE_LOCATION_FETCH',
-        url
-    }
-}
-var fetchLocation = () => {
-    store.dispatch(startLocationFetch());
-
-    axios.get('http://ipinfo.io').then(function(res){
-        var loc = res.data.loc;
-        var baseUrl = 'http://maps.google.com/?q='
-
-        store.dispatch(completeLocationFetch(baseUrl+loc));
-    })
-}
-
-var reducer = redux.combineReducers({
-    name: nameReducer,
-    hobbies: hobbiesReducer,
-    movies: moviesReducer,
-    map: mapReducer
-})
-
-var store = redux.createStore(reducer, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f  // f => f <<<is equal to>>> (f) => return f;
-));
+var actions = require('./actions/index')
+var store = require('./store/configureStore').configure();
 
 //Subscribe to changes
 var unsubscribe = store.subscribe(() => {
@@ -151,16 +22,16 @@ var currentState = store.getState();  //returns the STATE from the app, which is
 
 console.log("Current state", currentState);
 
-fetchLocation();
+store.dispatch(actions.fetchLocation());
 
-store.dispatch(changeName("Manuel"));
-store.dispatch(changeName('Otro nombre')) 
-store.dispatch(addHobby('Running'))
-store.dispatch(addMovie('Ironman','superheroes'))
-store.dispatch(changeName('Nombre que no se imprime'));
-store.dispatch(addHobby('Jogging'));
-store.dispatch(addMovie('Titanic','drama'));
-store.dispatch(removeHobby(2));
-store.dispatch(removeMovie(1));
+store.dispatch(actions.changeName("Manuel"));
+store.dispatch(actions.changeName('Otro nombre')) 
+store.dispatch(actions.addHobby('Running'))
+store.dispatch(actions.addMovie('Ironman','superheroes'))
+store.dispatch(actions.changeName('Nombre que no se imprime'));
+store.dispatch(actions.addHobby('Jogging'));
+store.dispatch(actions.addMovie('Titanic','drama'));
+store.dispatch(actions.removeHobby(2));
+store.dispatch(actions.removeMovie(1));
 
 
