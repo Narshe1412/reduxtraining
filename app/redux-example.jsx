ff609a555bc89e90b8ec73172af288c81e2a64be
Manuel <manuel1412@gmail.com>
2017-05-01 15:48:24 +0100

    add support for change name action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,10 +5,28 @@ console.log('Starting redux example');
 var reducer = (state = {name: 'Anonymous'}, action) => { //create a default argument in ES6
     //state = state || {name: 'Anonymous'}; // create a default argument ES5
 
-    return state;
+    switch(action.type) {
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+            }
+        default:
+            return state;
+    }
 }
+
 var store = redux.createStore(reducer); //create the state using the reducer, so the state that gets passed in doesn't get modified
 
 var currentState = store.getState();  //returns the STATE from the app, which is the Global Store (or global state)
 
 console.log("Current state", currentState);
+
+var action = {
+    type: 'CHANGE_NAME',   // only requirement is to have a type
+    name: 'Manuel'
+}
+
+store.dispatch(action);
+
+console.log("Name should be Manuel", store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -10,9 +10,26 @@ var stateDefault = {
 
 var reducer = (state = stateDefault, action) => { //create a default argument in ES6
     //state = state || stateDefault; // create a default argument ES5
-    return state;
+
+    switch(action.type){
+        case 'CHANGE_SEARCH_TEXT':
+            return {
+                ...state,
+                searchText: action.searchText
+            }
+        default:
+            return state;
+    }
 }
 
 var store = redux.createStore(reducer); //create the state using the reducer, so the state that gets passed in doesn't get modified
 
 console.log("Current state", store.getState());  //returns the STATE from the app, which is the Global Store (or global state)
+
+var changeSearchText = {
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'new text'
+}
+
+store.dispatch(changeSearchText);
+console.log("New search text", store.getState());
